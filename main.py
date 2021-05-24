54f400dc121172ac80b7d1c242dfa16dbf038afe
Farzan Khan <farzankhan009@gmail.com>
2021-05-23 17:06:21 -0800

    now receiving face array from module extract_faces

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+#History:
+#encountered many difficulties at generalizing extract_faces
+#module,  altered it back to original way of doing
+#now it return the only face in picture
+#so may expect change of logic in this module
+
+
 from extract_faces import extract_faces
 from os import listdir
 from numpy import asarray
@@ -9,8 +16,9 @@ picture_folder= 'pic_input/'
 # path
 path = picture_folder + "single-face-pic-input.jpg"
 # get face
+
 face_picture_input = extract_faces(path)
 #below plotting just for initial test purpose
 #print(face_picture_input)
-#pyplot.imshow(face_picture_input[0])
+#pyplot.imshow(face_picture_input)
 #pyplot.show()
