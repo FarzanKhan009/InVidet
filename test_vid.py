b8f6d72a5ff513faf51bcb9ee0c30403a025efab
Farzan Khan <farzankhan009@gmail.com>
2021-05-23 17:09:23 -0800

    replace dlib with mtcnn at face detection in video Dlib was very annoying ignored many frames MTCNN I would say 300 times good then Dlib barely ignored any face

--- test_vid.py
+++ test_vid.py
@@ -1,32 +1,50 @@
 import cv2
-import face_recognition
+from mtcnn_cv2 import MTCNN
 
+
+#History:
+#first implemented dlib face_recognition module detect faces
+#But dlibs' accuracy was very poor at detecting faces in video file
+#So followed the same i.e. mtcnn which used in detecting faces in picture
+#now removing all dlibs imports and scrap code in comments
+
+#opening video file using cv2
 cap= cv2.VideoCapture("vid_input/video.webm")
 
-face_locations= []
-
-count=0
+#loop to monitor enter key to terminate and iterate video frames
 while True:
     # Grab a single frame of video
     ret, frame = cap.read()
     # Convert the image from BGR color (which OpenCV uses) to RGB
-    # color (which face_recognition uses)
-    
-    try:
-        
-        rgb_frame = frame[:, :, ::-1]
-    except:
-        print("frame problem")
-        print(count)
-        count +=1
-        continue
-    # Find all the faces in the current frame of video
-    face_locations = face_recognition.face_locations(rgb_frame)
-    for top, right, bottom, left in face_locations:
-        # Draw a box around the face
-        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0,
-                                                            255), 2)
-        # Display the resulting image
+    # RGB is preferred color while detection faces
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    #another way of converting to RGB=>> rgb_frame = frame[:, :, ::-1]
+
+    #loading detector from MTCNN
+    detector = MTCNN()
+
+    #saving all faces in result variable
+    result = detector.detect_faces(rgb_frame)
+
+    #if only one face in the frames
+    print(result)
+
+    if len(result) > 0:
+        for face in result:
+            #if confused read the documentation of cv2.rectangle() it would help a lot
+
+            #starting point of the face is stored in x1, y1 as tupple i.e. (x1,y1)
+            x1, y1, width, height= result[0]['box']
+
+            #storing ending points in x2, y2
+            x2, y2, = x1+width, y1+height
+
+            #cv2.rectangle(start, end, color(RGB), (pixel of drawn line))
+            #drawing rectangle araound the face
+            cv2.rectangle(frame, (x1,y1),
+                            (x2,y2), (0, 155, 255),2)
+
+    # Display the resulting image
     cv2.imshow('Video', frame)
 
     # Wait for Enter key to stop
@@ -34,5 +52,6 @@ while True:
         break
 
 
+#releasing video and destroying windows
 cap.release()
 cv2.destroyAllWindows()
