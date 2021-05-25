7d08b7402959182d96e347d36e5f53a708e39945
Farzan Khan <farzankhan009@gmail.com>
2021-05-24 18:42:09 -0800

    testing with deepface to check performance of different models

---
+++ rec_by_deepface.py
@@ -0,0 +1,5 @@
+#It is testing module will implement deepface using different
+#models including faceNet, main purpose is to align the progress
+#and testing performance of different models
+
+#imports
