afec05470bb1419d8944efa9ea78ebdbfde01b86
Farzan Khan <farzankhan009@gmail.com>
2021-05-24 20:10:15 -0800

    extracted face from frames and saved these in video frames directory it was I guess necessary to work with deepface

--- test_vid.py
+++ test_vid.py
@@ -1,5 +1,8 @@
 import cv2
 from mtcnn_cv2 import MTCNN
+from numpy import asarray
+
+from PIL import Image
 
 
 #History:
@@ -7,15 +10,28 @@ from mtcnn_cv2 import MTCNN
 #But dlibs' accuracy was very poor at detecting faces in video file
 #So followed the same i.e. mtcnn which used in detecting faces in picture
 #now removing all dlibs imports and scrap code in comments
+#version 2:
+#changing some lines + adding some lines to extract video frames
+#to work with deepface
+
+
 
 #opening video file using cv2
 cap= cv2.VideoCapture("vid_input/multi-face.webm")
 
 #loop to monitor enter key to terminate and iterate video frames
-#countframes=0
+countframes=0
 while True:
     # Grab a single frame of video
     ret, frame = cap.read()
+    countframes += 1
+
+    #below if conditional logic is to boost up the speed
+    #reducing frames rate 6 fps actual was 30fps
+    if countframes %5 != 0:
+        continue
+
+
     # Convert the image from BGR color (which OpenCV uses) to RGB
     # RGB is preferred color while detection faces
     rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
@@ -31,27 +47,38 @@ while True:
     print(result)
 
     if len(result) > 0:
+        #to work with deep face
+        frame_array = asarray(frame)
+
         #taking variable i; to iterate in loop; for detecting multiple faces in single frame
-        i=0
+        face_num=0
         for face in result:
             #if confused read the documentation of cv2.rectangle() it would help a lot
 
             #starting point of the face is stored in x1, y1 as tupple i.e. (x1,y1)
-            x1, y1, width, height= result[i]['box']
+            x1, y1, width, height= result[face_num]['box']
 
             #storing ending points in x2, y2
             x2, y2, = x1+width, y1+height
+
+            #rather than drawing rectangle I would want to save faces
+            #appeared in frames to a directory; to work with deepface
+            frame_face = frame_array[y1:y2, x1:x2]
+            frame_face = Image.fromarray(frame_face)
+            face_name= f'{countframes}{"-"}{face_num}'
+            frame_path= "video_frames/"+ face_name+ ".jpg"
+            frame_face.save(frame_path)
 
             #cv2.rectangle(start, end, color(RGB), (pixel of drawn line))
             #drawing rectangle araound the face
             cv2.rectangle(frame, (x1,y1),
                             (x2,y2), (0, 155, 255),2)
-            i+= 1
+            face_num+= 1
 
     # Display the resulting image
     cv2.imshow('Video', frame)
 #    countframes+= 1
- #   print(countframes)
+    print(countframes)
 
     # Wait for Enter key to stop
     if cv2.waitKey(25) == 13:
