3bdfeb517734cf6b1881f11b94207685ee0fee14
Farzan Khan <farzankhan009@gmail.com>
2021-05-24 22:20:05 -0800

    Used FaceNet through DeepFace its either API or Something else 1st its is too slow at recognition i.e. returning rsults 2nd most of the time returns false for same person I guess FaceNet is not good enough will test some Other models in main in coming time...

--- main.py
+++ main.py
@@ -3,21 +3,42 @@
 #module,  altered it back to original way of doing
 #now it return the only face in picture
 #so may expect change of logic in this module
-
+import os
 
 from extract_faces import extract_faces
 from os import listdir
 from numpy import asarray
 from PIL import Image
-from matplotlib import pyplot
+import cv2
+from matplotlib import pyplot as plt
 from mtcnn.mtcnn import MTCNN
+
+#Imports to work with deep face
+import rec_by_deepface as df
+
+
+
 picture_folder= 'pic_input/'
 
 # path
 path = picture_folder + "single-face-pic-input.jpg"
 # get face
-
 face_picture_input = extract_faces(path)
+
+
+#going to work with deepface
+i=0
+for files in os.listdir("video_frames/"):
+    frames = cv2.imread("video_frames/%s" %(files))[:, :, ::-1]
+
+    try:
+        df.verify(path, frames)
+        i+= 1
+        print("no", i, "frame name: ", files)
+    except:
+        print("Face might not been detected for file %s" %(i))
+        i+= 1
+
 #below plotting just for initial test purpose
 #print(face_picture_input)
 #pyplot.imshow(face_picture_input)
