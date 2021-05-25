1d8603be15d427fab1dbe778258f2c498634f08b
Farzan Khan <farzankhan009@gmail.com>
2021-05-24 18:53:23 -0800

    Added verify() added function to recognition FaceNet model

--- rec_by_deepface.py
+++ rec_by_deepface.py
@@ -3,3 +3,15 @@
 #and testing performance of different models
 
 #imports
+from deepface import DeepFace
+import cv2
+import matplotlib.pyplot as plt
+
+def verify(img, frame):
+    results= DeepFace.verify(img, frame, model_name="Facenet")
+    print("result: ", results)
+    verification= results[0]
+    if verification:
+        print("they are same")
+    else:
+        print("not same")
