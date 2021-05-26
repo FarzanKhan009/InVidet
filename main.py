06e3ab77d06ed5d0ccdac4e49e72112d31a71f97
Farzan Khan <farzankhan009@gmail.com>
2021-05-25 21:37:29 -0800

    Tested DeepFace + Facenet corrected error hapening earlier at picture input as I was not reading and passing it but API or whatever you say about these are very slow thats you should be moving your own model or at least implement Facenet in at local

--- main.py
+++ main.py
@@ -3,33 +3,42 @@
 #module,  altered it back to original way of doing
 #now it return the only face in picture
 #so may expect change of logic in this module
-import os
 
-from extract_faces import extract_faces
-from os import listdir
-from numpy import asarray
-from PIL import Image
+
+import os
 import cv2
-from matplotlib import pyplot as plt
-from mtcnn.mtcnn import MTCNN
+
 
 #Imports to work with deep face
+import matplotlib.pyplot as plt
+
 import rec_by_deepface as df
 
 
 
 
-
-
+vf, fn, of, dff, did, af,dlb=1, 1,1,1,1,1,1
+picture= "extracted_face_picture/single_face_picture.jpg"
+picture = cv2.imread(picture)[:, :, ::-1]
 #going to work with deepface
 i=0
-for files in os.listdir("video_frames/"):
-    frames = cv2.imread("video_frames/%s" %(files))[:, :, ::-1]
+for files in os.listdir("extracted_faces_video_frames"):
+    frames = cv2.imread("extracted_faces_video_frames/%s" %(files))[:, :, ::-1]
+
+#    plt.imshow(picture)
+#    plt.show()
+#    break
 
     try:
-        df.verify(path, frames)
+        #vf= df.verify(picture, frames, "VGG-Face", vf, fn, of, dff, did, af,dlb)
+        fn= df.verify(picture,frames, "Facenet", vf, fn, of, dff, did, af,dlb)
+        #of= df.verify(picture, frames, "OpenFace", vf, fn, of, dff, did, af,dlb)
+        dff= df.verify(picture, frames, "DeepFace", vf, fn, of, dff, did, af,dlb)
+        did= df.verify(picture, frames, "DeepID", vf, fn, of, dff, did, af,dlb)
+        af= df.verify(picture, frames, "ArcFace", vf, fn, of, dff, did, af,dlb)
+        dlb= df.verify(picture, frames, "Dlib", vf, fn, of, dff, did, af,dlb)
         i+= 1
-        print("no", i, "frame name: ", files)
+        print(i, "Above reuslts are for frame", files)
     except:
         print("Face might not been detected for file %s" %(i))
         i+= 1
